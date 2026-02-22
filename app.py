5af9653e007ef1ae8abab6aedc54f7c88f016905
someonexunknown <131123356+someonexunknown@users.noreply.github.com>
2026-02-22 10:25:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def generate_inventory_trends(user_input, location):
     )
     try:
         response = client.models.generate_content(
-            model="gemma-3-27b-it",
+            model="gemma-3-12b-it",
             contents=prompt,
             config={"temperature": 0.1, "max_output_tokens": 100}
         )
